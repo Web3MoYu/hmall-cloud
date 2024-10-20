56c8520e37acc2098e2f847c9fa59fe4a353da41
lsh <3040114965@qq.com>
2024-10-20 16:45:46 +0800

    优化项目结构，将openFeign提称一个api模块

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+// 因为spring容器默认只会扫描启动类包下包及其子类，client扫描不出来，所以需要指定
+@EnableFeignClients(basePackages = "com.hmall.api.client")
 @SpringBootApplication
 public class CartApplication {
     public static void main(String[] args) {
