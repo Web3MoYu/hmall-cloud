8d72df99b14ca07e29bb6bc409125f3ff03a6489
lsh <3040114965@qq.com>
2024-10-23 17:04:08 +0800

    pay-service

---
+++ pay-service/src/main/java/com/hmall/pay/PayApplication.java
@@ -0,0 +1,13 @@
+package com.hmall.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "com.hmall.api")
+@SpringBootApplication
+public class PayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+}
