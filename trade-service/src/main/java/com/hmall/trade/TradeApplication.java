2b94f0806875ac95649779c131685da0f4df3519
lsh <3040114965@qq.com>
2024-10-23 16:40:19 +0800

    trade-service

---
+++ trade-service/src/main/java/com/hmall/trade/TradeApplication.java
@@ -0,0 +1,13 @@
+package com.hmall.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "com.hmall.api.client")
+@SpringBootApplication
+public class TradeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TradeApplication.class, args);
+    }
+}
