e177e609e6ffbcc79d2e2e4e638007ebb4dfefdb
lsh <3040114965@qq.com>
2024-10-23 16:12:59 +0800

    user-service

---
+++ user-service/src/main/java/com/hmall/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.hmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "com.hmall.api.client")
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
