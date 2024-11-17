9e920fd87c87de4beab91ebf4ad6f4b99799b26b
lsh <3040114965@qq.com>
2024-11-17 18:48:42 +0800

    索引库的增删查

--- item-service/src/test/java/com/hmall/item/es/ESTest.java
+++ item-service/src/test/java/com/hmall/item/es/ESTest.java
@@ -1,8 +1,13 @@
 package com.hmall.item.es;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +23,34 @@ public class ESTest {
         System.out.println("client: " + client);
     }
 
+    // 创建索引库
+    @Test
+    void testCreateIndex() throws IOException {
+        // 1.准备Request对象
+        CreateIndexRequest request = new CreateIndexRequest("items");
+        // 2.准备请求参数
+        request.source(MAPPING_TEMPLATE, XContentType.JSON);
+        // 3.发送请求
+        client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testGetIndex() throws IOException {
+        // 1.准备Request对象
+        GetIndexRequest request = new GetIndexRequest("items");
+        // 2.发送请求
+        boolean response = client.indices().exists(request, RequestOptions.DEFAULT);
+        System.out.println(response);
+    }
+
+    @Test
+    void testDeleteIndex() throws IOException {
+        // 1.准备Request对象
+        DeleteIndexRequest request = new DeleteIndexRequest("items");
+        // 2.发送请求
+        client.indices().delete(request, RequestOptions.DEFAULT);
+    }
+
 
     @BeforeEach
     public void setUp() {
@@ -31,4 +64,44 @@ public class ESTest {
             client.close();
         }
     }
+
+    private static final String MAPPING_TEMPLATE = "{\n" +
+            "  \"mappings\": {\n" +
+            "    \"properties\": {\n" +
+            "      \"id\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"name\": {\n" +
+            "        \"type\": \"text\",\n" +
+            "        \"analyzer\": \"ik_smart\"\n" +
+            "      },\n" +
+            "      \"price\": {\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"image\": {\n" +
+            "        \"type\": \"keyword\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"category\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"brand\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"sold\": {\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"commentCount\": {\n" +
+            "        \"type\": \"integer\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"isAD\": {\n" +
+            "        \"type\": \"boolean\"\n" +
+            "      },\n" +
+            "      \"updateTime\": {\n" +
+            "        \"type\": \"date\"\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
 }
